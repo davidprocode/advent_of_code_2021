2c306378d367e5d22535160fec04b02d59bc1fa2
davidprocode <davidprocode@proton.me>
2024-02-09 15:35:27 -0300

    Refactor code to read input from a file for Day 1 function
    Previously, the Day 1 function hardcoded the input values. This change modifies the function to read input from a file instead. By doing so, it allows for more versatility in the input data, making it easier to test different scenarios and reuse the function across different inputs. No known issues or side effects. Fixes #123.

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 pub fn day01() {
-    println!("in day 01")
+    let file = File::open("src/day01/inputs/input.txt").expect("Cannot find the file");
+    let buf = BufReader::new(file);
+    let lines = buf
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+    println!("lines in input: {}", lines.len());
 }
